5a6b4e5c90dae4d6d896430336b177fa1dbe01b5
Anton Malynevskyi <malina.elfg@gmail.com>
2024-01-12 23:47:36 +0100

    Correcred Task1 and Task2. Done with varargs

--- DiscordTasks/src/recursion/Methods.java
+++ DiscordTasks/src/recursion/Methods.java
@@ -1,40 +1,70 @@
 package recursion;
 
 public class Methods {
-    public static void concatination(StringBuilder result, String input){
-        result.append(input).append(" ");
+    public static String concatination(String ...strings){
+        if (strings.length == 0){
+            return "";
+        }
+        String result = "";
+        for (String x: strings)
+            result += x + " ";
+        return result.trim();
     }
 
-    public static int sum(int a, int b){
-        return a + b;
+    public static int sum(int ...numbers){
+        int sum = 0;
+        for (int x: numbers)
+            sum += x;
+        return sum;
     }
 
-    public static float sum(float a, float b){
-        return a + b;
+    public static float sum(float ...numbers){
+        float sum = 0;
+        for (float x: numbers)
+            sum += x;
+        return sum;
     }
 
-    public static double sum (double a, double b){
-        return a + b;
+    public static double sum (double ...numbers){
+        double sum = 0;
+        for (double x: numbers)
+            sum += x;
+        return sum;
     }
 
-    public static long sum (long a,long b){
-        return a + b;
+    public static long sum (long ...numbers){
+        long sum = 0;
+        for (long x: numbers)
+            sum += x;
+        return sum;
     }
 
-    public static short sum (short a, short b){
-        return (short) (a + b);
+    public static short sum (short ...numbers){
+        short sum = 0;
+        for (short x: numbers)
+            sum += x;
+        return sum;
     }
 
-    public static byte sum (byte a, byte b){
-        return (byte) (a + b);
+    public static byte sum (byte ...numbers){
+        byte sum = 0;
+        for (byte x: numbers)
+            sum += x;
+        return sum;
     }
 
-    public static String sum (char a, char b){
-        return String.valueOf(a) + b;
+    public static String sum (char ...chars){
+        String result = "";
+        for (char x: chars)
+            result += x;
+        return result;
     }
 
-    public static boolean sum (boolean a, boolean b){
-        return a && b;
+    public static boolean sum (boolean ...booleans){
+        boolean result = true;
+        for (boolean x: booleans)
+            result = result && x;
+        return result;
     }
 
     public static long factorial(long a){
--- DiscordTasks/src/recursion/Task1.java
+++ DiscordTasks/src/recursion/Task1.java
@@ -6,15 +6,7 @@ import java.io.InputStreamReader;
 
 public class Task1 {
     public static void main(String[] args) throws IOException {
-        StringBuilder result = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String input;
-        System.out.println("Type Strings. If you want to stop - type space and nothing else");
-        input = reader.readLine();
-        while (!input.isEmpty()){
-            Methods.concatination(result, input);
-            input = reader.readLine();
-        }
-        System.out.println(result.toString().trim());
+        String result = Methods.concatination("Hello", "Hi", "Nihao");
+        System.out.println(result);
     }
 }
